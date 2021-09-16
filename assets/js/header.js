c0d198c7b49b710fa92ee29e55cc520d8f5a1e3f
MelonKony <melonkony@icloud.com>
2021-09-16 17:00:16 +1000

    Disabled dark mode (for now)

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,10 +13,10 @@ window.addEventListener("DOMContentLoaded", function () {
     const dark_mode_btn = document.getElementById("dark_mode_btn");
     const light_mode_btn = document.getElementById("light_mode_btn");
 
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        light_mode_btn.classList.remove('hidden');
-    } else {
-        dark_mode_btn.classList.remove('hidden');
+    //if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        //light_mode_btn.classList.remove('hidden');
+    //} else {
+      //dark_mode_btn.classList.remove('hidden');
     }
 
     dark_mode_btn.addEventListener('click', function () {
